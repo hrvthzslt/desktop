bb13feafae42a253812122ea79aa68da6063660d
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-04-19 12:21:27 +0200

    Feat/sxhkd (#3)

--- roles/dwm/files/dwm/autostart.sh
+++ roles/dwm/files/dwm/autostart.sh
@@ -2,6 +2,7 @@
 
 gnome-keyring-daemon -r -d &
 autorandr --change --force && nitrogen --restore;
+sxhkd &
 slstatus &
 clipmenud &
 picom --backend glx --vsync --blur-method dual_kawase --blur-strength 4 &
