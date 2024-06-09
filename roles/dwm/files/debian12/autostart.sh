b6945087746ec039763d798d8e95c7ccb07e3bbc
Zsolt Horváth <hrth.7lst@gmail.com>
2024-06-09 12:51:49 +0200

    feat(dwm): use different autostart with debian 12

---
+++ roles/dwm/files/debian12/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+gnome-keyring-daemon -r -d &
+autorandr --change --force && nitrogen --restore;
+sxhkd &
+slstatus &
+clipmenud &
+picom --backend glx --vsync --blur-method dual_kawase --blur-strength 4 --experimental-backends &
+dunst &
