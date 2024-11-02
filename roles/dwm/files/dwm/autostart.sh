224e84f12d5c6f586013e6827fad1b6fad2c9943
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-11-02 15:12:26 +0100

    autostart dwmblocks

--- roles/dwm/files/dwm/autostart.sh
+++ roles/dwm/files/dwm/autostart.sh
@@ -3,7 +3,7 @@
 gnome-keyring-daemon -r -d &
 autorandr --change --force && nitrogen --restore;
 sxhkd &
-slstatus &
+dwmblocks &
 clipmenud &
 picom --backend glx --vsync --blur-method dual_kawase --blur-strength 4 &
 dunst &
