f4dc04ce15643849e09693520c7c7a80cb1c2b6f
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-11-02 14:58:36 +0100

    blocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,19 +1,24 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" 󱌼 ", "free | grep Mem | awk '{printf \"%.0f%%\", $3/$2 * 100.0}'", 1, 0},
-    {" ", "iostat -c | awk 'NR>= 4 && NR<=4' | awk '{printf \"%.0f%%\", $1}'",	1, 0},
-    {" ", "df / | grep / | awk '{print $5}'", 1, 0},
+	{" 󱌼 ", "mem_perc", 1, 0},
+    {"  ", "cpu_perc",	1, 0},
+    {"  ", "disk_perc", 1, 0},
 
     {"", "", 0, 0},
 
-    {"", "[ \"$(clipctl status)\" = 'enabled' ] && echo 󰅎 || echo 󱘜", 0, 10},
-    {" ", "[ \"$(dunstctl is-paused)\" = 'false' ] && echo 󰎚 || echo 󱞃", 0, 12},
+    {"", "clipctl_status", 0, 10},
+    {" ", "dunst_status", 0, 12},
 
-    {"  ", "amixer sget Master | tail -1 | awk '{print $5 }' | sed 's@\\(\\[\\|\\]\\)@@g'", 0, 14},
-    /* {"  ", 'nmcli dev wifi | grep "\*" | awk '{print $8}'\, 0, 14}, */
+    {"  ", "alsa_perc", 0, 14},
+    {" 󰂯 ", "bluetooth_count", 10, 0},
+    {" 󱊣 ", "battery", 60, 0},
 
-	{"", "date '+%Y-%m-%d %H:%M:%S'", 1, 0},
+    {"", "", 0, 0},
+
+	{"", "date_hour", 1, 0},
+
+    {" ", "", 0, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
