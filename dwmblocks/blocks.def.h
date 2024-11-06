47754eec70603cb98fc3863520d4edfa1de8e9a3
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-11-06 15:44:01 +0100

    fix: dwmblock start and rename scripts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,23 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" 󱌼 ", "mem_perc", 1, 0},
-    {"  ", "cpu_perc",	1, 0},
-    {"  ", "disk_perc", 1, 0},
+	{" 󱌼 ", "mem_perc_block", 1, 0},
+    {"  ", "cpu_perc_block",	1, 0},
+    {"  ", "disk_perc_block", 1, 0},
 
     {"", "", 0, 0},
 
-    {"", "clipctl_status", 0, 10},
-    {" ", "dunst_status", 0, 12},
+    {"", "clipctl_status_block", 0, 10},
+    {" ", "dunst_status_block", 0, 12},
 
-    {"  ", "alsa_perc", 10, 14},
-    {" 󰖩 ", "wifi_perc", 10, 0},
-    {" 󰂯 ", "bluetooth_count", 10, 0},
-    {" 󱊣 ", "battery", 60, 0},
+    {"  ", "alsa_perc_block", 10, 14},
+    {" 󰖩 ", "wifi_perc_block", 10, 0},
+    {" 󰂯 ", "bluetooth_count_block", 10, 0},
+    {" 󱊣 ", "battery_block", 60, 0},
 
     {"", "", 0, 0},
 
-	{"", "date_hour", 1, 0},
+	{"", "date_hour_block", 1, 0},
 
     {" ", "", 0, 0},
 };
--- roles/dwm/files/debian12/autostart.sh
+++ roles/dwm/files/debian12/autostart.sh
@@ -3,7 +3,7 @@
 gnome-keyring-daemon -r -d &
 autorandr --change --force && nitrogen --restore;
 sxhkd &
-dwmlblocks &
+dwmblocks &
 clipmenud &
 picom --backend glx --vsync --blur-method dual_kawase --blur-strength 4 --experimental-backends &
 dunst &
