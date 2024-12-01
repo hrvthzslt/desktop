1d087fc281a8c3fd61c18d45b9e9a4c2f9c8889f
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-12-01 09:54:57 +0100

    feat: control dmenu colors from outside

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -9,9 +9,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#CFC1BA", "#1C1917" },
-	[SchemeSel] = { "#F0EDEC", "#2C363C" },
-	[SchemeOut] = { "#F0EDEC", "#2C363C" },
+	/* [SchemeNorm] = { "#CFC1BA", "#1C1917" }, */
+	/* [SchemeSel] = { "#F0EDEC", "#2C363C" }, */
+	/* [SchemeOut] = { "#F0EDEC", "#2C363C" }, */
+    [SchemeNorm] = { "#bbbbbb", "#222222" },
+    [SchemeSel] = { "#eeeeee", "#005577" },
+    [SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
