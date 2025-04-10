8a9518952f259d26f57df57376b5d0d21313f255
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-04-10 21:25:32 +0200

    feat: separate picom command and configuration (#10)
    * feat: separate picom command and configuration
    
    * fix: cleanup dwm role

--- roles/dwm/files/dwm/autostart.sh
+++ roles/dwm/files/dwm/autostart.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+picom_version=$(picom --version)
+
 gnome-keyring-daemon -r -d &
 autorandr --change --force && nitrogen --restore;
 sxhkd &
 dwmblocks &
 clipmenud &
-picom --backend glx --vsync --blur-method dual_kawase --blur-strength 4 &
+if [ "$picom_version" = 'v9.1' ]; then
+    picom --experimental-backends &
+else
+    picom &
+fi
 dunst &
 sh ~/.dwm/poststart.sh 2>/dev/null &
