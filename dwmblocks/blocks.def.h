4f88494ef9dd1accc8939da06dc3f59c9147edd1
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-05-24 19:13:43 +0200

    feat: make status scripts fault tolerant

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" 󱌼 ", "mem_perc_block", 1, 0},
-    {"  ", "cpu_perc_block",	1, 0},
-    {"  ", "disk_perc_block", 1, 0},
+	{"", "mem_perc_block", 1, 0},
+    {"", "cpu_perc_block",	1, 0},
+    {"", "disk_perc_block", 1, 0},
 
     {"", "", 0, 0},
 
     {"", "clipctl_status_block", 0, 10},
     {" ", "dunst_status_block", 0, 12},
 
-    {"  ", "alsa_perc_block", 5, 14},
-    {" 󰖩 ", "wifi_perc_block", 5, 0},
-    {" 󰂯 ", "bluetooth_count_block", 5, 0},
-    {" 󱊣 ", "battery_block", 1, 0},
+    {"", "alsa_perc_block", 5, 14},
+    {"", "wifi_perc_block", 5, 0},
+    {"", "bluetooth_count_block", 5, 0},
+    {"", "battery_block", 1, 0},
     {"", "power get", 0, 16},
 
     {"", "", 0, 0},
