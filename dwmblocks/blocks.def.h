2eb916aeabdef2992528dffae917c6d2fad38b53
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-11-23 17:40:12 +0100

    feat: keymap script and dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -14,6 +14,7 @@ static const Block blocks[] = {
     {"", "wifi_perc_block", 5, 0},
     {"", "bluetooth_count_block", 5, 0},
     {"", "battery_block", 1, 0},
+    {"", "keymap icon", 0, 18},
     {"", "power get", 0, 16},
 
     {"", "", 0, 0},
